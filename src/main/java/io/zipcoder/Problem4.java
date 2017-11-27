e54389e607c330ef61d13c9694d8476fd2af5606
logan <loganhanner@gmail.com>
2017-11-27 15:53:13 -0500

    This feels really slippery...

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem4 {
+
+    public String canBePalindrome(String input) {
+
+        String returnValue = "NO";
+
+        String[] splitInput = input.split("");
+        List<String> letters = Arrays.stream(splitInput).distinct().collect(Collectors.toList());
+        Integer[] letterCount = new Integer[letters.size()];
+
+        for(int i = 0; i < letters.size(); i++) {
+            letterCount[i] = 0;
+            for(int j = 0; j < splitInput.length; j++) {
+                if(splitInput[j].equals(letters.get(i))) {
+                    letterCount[i]++;
+                }
+            }
+        }
+
+        int odds = 0;
+
+        for(int i = 0; i < letterCount.length; i++) {
+            if(letterCount[i] % 2 != 0) {
+                odds++;
+            }
+        }
+
+        if(odds <= 1) {
+            returnValue = "YES";
+        }
+
+        return returnValue;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,36 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    Problem4 problem4;
+
+    @Before
+    public void setup() {
+        problem4 = new Problem4();
+    }
+
+
+    @Test
+    public void canBePalindrome_YES() {
+        String input = "aaabbbb";
+
+        String expected = "YES";
+        String actual = problem4.canBePalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canBePalindrome_NO() {
+        String input = "cdefghmnopqrstuvw";
+
+        String expected = "NO";
+        String actual = problem4.canBePalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
